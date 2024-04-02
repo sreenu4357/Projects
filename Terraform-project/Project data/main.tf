3325f11b153c14966b24ae33dac6454933e6d2ca
sreenu4357 <141114849+sreenu4357@users.noreply.github.com>
2024-04-02 18:07:39 -0400

    Create main.tf

---
+++ Terraform-project/Project data/main.tf
@@ -0,0 +1,34 @@
+
+resource "aws_launch_configuration" "web_server_as" {
+    image_id           = "ami-0c101f26f147fa7fd"
+    instance_type = "t2.micro"
+    key_name = "sreenu"
+}
+   
+
+
+  resource "aws_elb" "web_server_lb"{
+     name = "web-server-lb"
+     security_groups = [aws_security_group.web_server.id]
+     subnets = ["subnet-08ccf95e25657ee53", "subnet-013272d01c5dd9f78"]
+     listener {
+      instance_port     = 8000
+      instance_protocol = "http"
+      lb_port           = 80
+      lb_protocol       = "http"
+    }
+    tags = {
+      Name = "terraform-elb"
+    }
+  }
+resource "aws_autoscaling_group" "web_server_asg" {
+    name                 = "web-server-asg"
+    launch_configuration = aws_launch_configuration.web_server_as.name
+    min_size             = 1
+    max_size             = 3
+    desired_capacity     = 2
+    health_check_type    = "EC2"
+    load_balancers       = [aws_elb.web_server_lb.name]
+    availability_zones    = ["us-east-1d", "us-east-1f"] 
+    
+  }
